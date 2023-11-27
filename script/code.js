3e756730e22225c2a30ab39fe6ba9eba944b209b
nathaliewillemse21 <njpwillemse69@gmail.com>
2023-11-27 18:28:10 +0200

    done

---
+++ script/code.js
@@ -0,0 +1,11 @@
+
+let currentNumber = 0;
+
+function nextNumber() {
+    currentNumber ++;
+    return currentNumber;
+}
+
+
+console.log("Current Number:", currentNumber);
+console.log("Next Number:", nextNumber());
